09f5d332253438ca82f7b116cac2fef54019bc84
hansh0101 <hansh0101@naver.com>
2022-07-10 05:09:10 +0900

    [FEATURE] #23 - Onboarding1Fragment initAnimation() 구현

--- app/src/main/java/co/kr/deartoday/presentation/ui/onboarding/Onboarding1Fragment.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/onboarding/Onboarding1Fragment.kt
@@ -1,14 +1,17 @@
 package co.kr.deartoday.presentation.ui.onboarding
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import co.kr.deartoday.R
 import co.kr.deartoday.databinding.FragmentOnboarding1Binding
 import co.kr.deartoday.presentation.ui.base.BaseFragment
+import co.kr.deartoday.util.fadeInAnimator
+import co.kr.deartoday.util.slideUpAnimator
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class Onboarding1Fragment : BaseFragment<FragmentOnboarding1Binding>() {
     override val TAG: String
@@ -20,6 +23,33 @@ class Onboarding1Fragment : BaseFragment<FragmentOnboarding1Binding>() {
         super.onViewCreated(view, savedInstanceState)
         binding.btnNext.setOnClickListener {
             findNavController().navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment)
+        }
+        initAnimation()
+    }
+
+    private fun initAnimation() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val fadeIn1 = fadeInAnimator(binding.tvOb1Line1, 1000)
+            val slideUp1 = slideUpAnimator(binding.tvOb1Line1, 1000)
+            binding.tvOb1Line1.visibility = View.VISIBLE
+            fadeIn1.start()
+            slideUp1.start()
+
+            delay(1000)
+
+            val fadeIn2 = fadeInAnimator(binding.tvOb1Line2, 1000)
+            val slideUp2 = slideUpAnimator(binding.tvOb1Line2, 1000)
+            binding.tvOb1Line2.visibility = View.VISIBLE
+            fadeIn2.start()
+            slideUp2.start()
+
+            delay(1000)
+
+            val fadeIn3 = fadeInAnimator(binding.btnNext, 1000)
+            binding.btnNext.visibility = View.VISIBLE
+            fadeIn3.start()
+
+            binding.btnNext.isClickable = true
         }
     }
 }
