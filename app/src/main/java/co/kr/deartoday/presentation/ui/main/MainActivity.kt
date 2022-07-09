cb37f818825869b0bc88978239bb8e61fb896c3b
hansh0101 <hansh0101@naver.com>
2022-07-09 19:43:00 +0900

    [FEATURE] #10 - MainViewPager 추가

--- app/src/main/java/co/kr/deartoday/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/main/MainActivity.kt
@@ -1,10 +1,27 @@
 package co.kr.deartoday.presentation.ui.main
 
+import android.os.Bundle
 import co.kr.deartoday.R
 import co.kr.deartoday.databinding.ActivityMainBinding
+import co.kr.deartoday.presentation.adapter.MainViewPagerAdapter
 import co.kr.deartoday.presentation.ui.base.BaseActivity
 
 class MainActivity() : BaseActivity<ActivityMainBinding>() {
     override val layoutRes: Int
         get() = R.layout.activity_main
+    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        mainViewPagerAdapter = MainViewPagerAdapter(this)
+        binding.vpMain.adapter = mainViewPagerAdapter
+    }
 }
