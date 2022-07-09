016cd7d5bd8b15faf55f6f554021fc7985c5bb63
uxri <yuree0217@g.eulji.ac.kr>
2022-07-10 02:31:50 +0900

    [FEATURE] #6 - Add Onboarding Activity

---
+++ app/src/main/java/co/kr/deartoday/presentation/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package co.kr.deartoday.presentation.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.kr.deartoday.R
+import co.kr.deartoday.databinding.ActivityOnboardingBinding
+import co.kr.deartoday.presentation.ui.base.BaseActivity
+
+class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.activity_onboarding
+}
