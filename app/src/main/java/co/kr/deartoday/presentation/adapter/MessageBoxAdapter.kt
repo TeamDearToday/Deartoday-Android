e9d6bdb512238d9dffd7078d0a77e34103b2295f
uxri <yuree0217@g.eulji.ac.kr>
2022-07-13 18:44:27 +0900

    [FEATURE] Max Line Calculation

--- app/src/main/java/co/kr/deartoday/presentation/adapter/MessageBoxAdapter.kt
+++ app/src/main/java/co/kr/deartoday/presentation/adapter/MessageBoxAdapter.kt
@@ -1,10 +1,12 @@
 package co.kr.deartoday.presentation.adapter
 
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import co.kr.deartoday.data.model.response.ResponseMessageBox
 import co.kr.deartoday.databinding.ItemMessageBoxListBinding
+import co.kr.deartoday.util.calculateMaxLines
 
 class MessageBoxAdapter : RecyclerView.Adapter<MessageBoxAdapter.MessageBoxViewHolder>() {
     val MessageBoxList = mutableListOf<ResponseMessageBox>()
@@ -13,7 +15,13 @@ class MessageBoxAdapter : RecyclerView.Adapter<MessageBoxAdapter.MessageBoxViewH
         private val binding: ItemMessageBoxListBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: ResponseMessageBox) {
-            binding.tvMessage.text = data.message
+            binding.tvMessageItem.text = data.message
+            binding.tvMessageItem.calculateMaxLines(data.message) {
+                    if (it >= 0) {
+                        binding.tvMessageItem.maxLines = it - 1
+                        binding.tvMessageItem.ellipsize = TextUtils.TruncateAt.END
+                    }
+                }
         }
     }
 
