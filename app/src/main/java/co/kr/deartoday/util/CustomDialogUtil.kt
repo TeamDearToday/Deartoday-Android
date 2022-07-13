2310e9a7d3f37300b5457b1c5f92b09a15810c01
hansh0101 <hansh0101@naver.com>
2022-07-13 22:09:15 +0900

    [FEATURE] #48 - CustomDialogUtil 구현

---
+++ app/src/main/java/co/kr/deartoday/util/CustomDialogUtil.kt
@@ -0,0 +1,27 @@
+package co.kr.deartoday.util
+
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import co.kr.deartoday.R
+
+class CustomDialog(val context: Context) {
+    private val dialog = Dialog(context)
+
+    fun showConfirmDialog(@LayoutRes layout: Int, inset: Int, content: String) {
+        dialog.setContentView(layout)
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawableResource(inset)
+        dialog.findViewById<TextView>(R.id.tv_content).text = content
+        dialog.show()
+        dialog.findViewById<View>(R.id.view_ok).setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+}
