09dd474678a44beaf7eb74cf5b42bde71cf9b531
Choi Woo hyeong <gua05093@gmail.com>
2022-07-14 02:05:36 +0900

    [FIX] 유리와 협의하에 itemlayout naming 수정

--- app/src/main/java/co/kr/deartoday/presentation/adapter/MessageBoxAdapter.kt
+++ app/src/main/java/co/kr/deartoday/presentation/adapter/MessageBoxAdapter.kt
@@ -4,13 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import co.kr.deartoday.data.model.response.ResponseMessageBox
-import co.kr.deartoday.databinding.ItemMessageBoxListBinding
+import co.kr.deartoday.databinding.ItemMessageBoxBinding
 
 class MessageBoxAdapter : RecyclerView.Adapter<MessageBoxAdapter.MessageBoxViewHolder>() {
     val MessageBoxList = mutableListOf<ResponseMessageBox>()
 
     class MessageBoxViewHolder(
-        private val binding: ItemMessageBoxListBinding
+        private val binding: ItemMessageBoxBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: ResponseMessageBox) {
             binding.tvMessage.text = data.message
@@ -19,7 +19,7 @@ class MessageBoxAdapter : RecyclerView.Adapter<MessageBoxAdapter.MessageBoxViewH
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageBoxViewHolder {
         val binding =
-            ItemMessageBoxListBinding.inflate(
+            ItemMessageBoxBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
