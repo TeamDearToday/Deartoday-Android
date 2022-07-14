6010f64f89b91fd35a57d7efeef150fe573caf49
Choi Woo hyeong <gua05093@gmail.com>
2022-07-14 23:48:44 +0900

    [FEATURE] #57 - 서버 데이터 형태로 더미 데이터 테스트

--- app/src/main/java/co/kr/deartoday/presentation/ui/timetravel/TimeTravelActivity.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/timetravel/TimeTravelActivity.kt
@@ -2,14 +2,48 @@ package co.kr.deartoday.presentation.ui.timetravel
 
 import android.os.Bundle
 import co.kr.deartoday.R
+import co.kr.deartoday.data.model.response.ResponseTimeTravelTape
 import co.kr.deartoday.databinding.ActivityTimeTravelBinding
+import co.kr.deartoday.presentation.adapter.TimeTravelTapeAdapter
 import co.kr.deartoday.presentation.ui.base.BaseActivity
+import co.kr.deartoday.util.MessageBoxItemDecoration
 
 class TimeTravelActivity : BaseActivity<ActivityTimeTravelBinding>() {
+    private lateinit var timeTravelTapeAdapter: TimeTravelTapeAdapter
     override val layoutRes: Int
         get() = R.layout.activity_time_travel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        binding.rvTimeTravel.adapter = timeTravelTapeAdapter
+        timeTravelTapeAdapter.tapeList.addAll(
+            listOf(
+                ResponseTimeTravelTape(
+                    "asfjkl1nbbj3j2343284bcjksabfd",
+                    "어쩔티비",
+                    2022,
+                    7,
+                    22,
+                    "2022.07.09",
+                    "이미지URL"
+                ),
+                ResponseTimeTravelTape(
+                    "asfjkl1nbbj3j2343284bcjksabfd",
+                    "어쩔티비",
+                    2022,
+                    7,
+                    22,
+                    "2022.07.09",
+                    "이미지URL"
+                )
+
+            )
+        )
+        timeTravelTapeAdapter.notifyDataSetChanged()
     }
 }
