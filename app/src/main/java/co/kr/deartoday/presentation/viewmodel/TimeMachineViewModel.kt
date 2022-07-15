25737fc999822decb1b6868b881629acb049606f
hansh0101 <hansh0101@naver.com>
2022-07-16 01:26:26 +0900

    [FEATURE] #51 - TimeMachineViewModel 구현
    - imageUri, date, title LiveData 추가
    - isImagePickerProcessComplete MediatorLiveData 추가
    - 필수 값 충족 조건 검사 로직 구현

---
+++ app/src/main/java/co/kr/deartoday/presentation/viewmodel/TimeMachineViewModel.kt
@@ -0,0 +1,29 @@
+package co.kr.deartoday.presentation.viewmodel
+
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TimeMachineViewModel : ViewModel() {
+    // TimeMachineImagePickerFragment
+    val imageUri = MutableLiveData<Uri>()
+    val date = MutableLiveData<String>()
+    val title = MutableLiveData<String>()
+    private val _isImagePickerProcessComplete = MediatorLiveData<Boolean>()
+    val isImagePickerProcessComplete: LiveData<Boolean> get() = _isImagePickerProcessComplete
+
+    init {
+        with(_isImagePickerProcessComplete) {
+            addSource(imageUri) { checkImagePickerProcessComplete() }
+            addSource(date) { checkImagePickerProcessComplete() }
+            addSource(title) { checkImagePickerProcessComplete() }
+        }
+    }
+
+    private fun checkImagePickerProcessComplete() {
+        _isImagePickerProcessComplete.value =
+            (imageUri.value != null) && (date.value != null) && (title.value != null && title.value != "")
+    }
+}
