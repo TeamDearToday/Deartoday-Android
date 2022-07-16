cc6e6cc4d923e1bb5b1fa56ca59d94100b017452
Choi Woo hyeong <gua05093@gmail.com>
2022-07-16 17:18:13 +0900

    [FIX] #67 - rewind_disable 추가 / 필요한 부분 교체

---
+++ app/src/main/java/co/kr/deartoday/presentation/ui/timetravel/TimeTravelDetailActivity.kt
@@ -0,0 +1,17 @@
+package co.kr.deartoday.presentation.ui.timetravel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.kr.deartoday.R
+import co.kr.deartoday.databinding.ActivityTimeTravelDetailBinding
+import co.kr.deartoday.presentation.ui.base.BaseActivity
+
+class TimeTravelDetailActivity : BaseActivity<ActivityTimeTravelDetailBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.activity_time_travel_detail
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
