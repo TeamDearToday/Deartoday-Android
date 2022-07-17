0f1bc0fd98e2553a247b4d614595249b0dc8cb61
uxri <yuree0217@g.eulji.ac.kr>
2022-07-18 03:17:19 +0900

    [FEATURE] recyclerView

--- app/src/main/java/co/kr/deartoday/presentation/ui/messagebox/MessageBoxActivity.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/messagebox/MessageBoxActivity.kt
@@ -3,22 +3,26 @@ package co.kr.deartoday.presentation.ui.messagebox
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import co.kr.deartoday.R
-import co.kr.deartoday.data.model.response.MessageBoxResponse
 import co.kr.deartoday.databinding.ActivityMessageBoxBinding
 import co.kr.deartoday.presentation.adapter.MessageBoxAdapter
 import co.kr.deartoday.presentation.ui.base.BaseActivity
 import co.kr.deartoday.presentation.ui.main.MainActivity
+import co.kr.deartoday.presentation.viewmodel.MessageBoxViewModel
 import co.kr.deartoday.util.CustomDialog
 import co.kr.deartoday.util.MessageBoxItemDecoration
 
 class MessageBoxActivity : BaseActivity<ActivityMessageBoxBinding>() {
     private lateinit var messageBoxAdapter: MessageBoxAdapter
+    private val viewModel by viewModels<MessageBoxViewModel>()
     override val layoutRes: Int
         get() = R.layout.activity_message_box
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.viewmodel = viewModel
 
         backBtnClickEvent()
         timeTravelBtnClickEvent()
@@ -50,17 +54,17 @@ class MessageBoxActivity : BaseActivity<ActivityMessageBoxBinding>() {
         binding.rvMessage.adapter = messageBoxAdapter
         messageBoxAdapter.messageBoxList.addAll(
             listOf(
-                MessageBoxResponse("유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? "),
-                MessageBoxResponse("하냥이"),
-                MessageBoxResponse("하연자"),
-                MessageBoxResponse("하하여자"),
-                MessageBoxResponse("디어투데이"),
-                MessageBoxResponse("하별찬"),
-                MessageBoxResponse("하메피"),
-                MessageBoxResponse("하피엠"),
-                MessageBoxResponse("하헬창"),
-                MessageBoxResponse("하남자"),
-                MessageBoxResponse("쌉하남자")
+                "유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? 유리는 하여자다! 아닌데? ",
+                "하냥이",
+                "하연자",
+                "하하여자",
+                "디어투데이",
+                "하별찬",
+                "하메피",
+                "하피엠",
+                "하헬창",
+                "하남자",
+                "쌉하남자"
             )
         )
 
