35ce80594ce3038bb4754e9be5c3631c6fd9b625
uxri <yuree0217@g.eulji.ac.kr>
2022-07-17 18:45:41 +0900

    [CHORE] Rename response

--- app/src/main/java/co/kr/deartoday/presentation/adapter/MessageBoxAdapter.kt
+++ app/src/main/java/co/kr/deartoday/presentation/adapter/MessageBoxAdapter.kt
@@ -4,27 +4,27 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import co.kr.deartoday.data.model.response.ResponseMessageBox
+import co.kr.deartoday.data.model.response.MessageBoxResponse
 import co.kr.deartoday.databinding.ItemMessageBoxListBinding
 import co.kr.deartoday.util.calculateMaxLines
 
 class MessageBoxAdapter(private val itemClick: (String) -> Unit) :
     RecyclerView.Adapter<MessageBoxAdapter.MessageBoxViewHolder>() {
-    val messageBoxList = mutableListOf<ResponseMessageBox>()
+    val messageBoxList = mutableListOf<MessageBoxResponse>()
 
     class MessageBoxViewHolder(
         private val binding: ItemMessageBoxListBinding,
         private val itemClick: (String) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: ResponseMessageBox) {
-            binding.tvMessageItem.text = data.message
-            binding.tvMessageItem.calculateMaxLines(data.message) {
+        fun onBind(data: MessageBoxResponse) {
+            binding.tvMessageItem.text = data.lastAnswers
+            binding.tvMessageItem.calculateMaxLines(data.lastAnswers) {
                 if (it >= 0) {
                     binding.tvMessageItem.maxLines = it - 1
                     binding.tvMessageItem.ellipsize = TextUtils.TruncateAt.END
                 }
             }
-            binding.root.setOnClickListener { itemClick(data.message) }
+            binding.root.setOnClickListener { itemClick(data.lastAnswers) }
         }
     }
 
