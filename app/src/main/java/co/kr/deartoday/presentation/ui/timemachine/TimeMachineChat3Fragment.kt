86936ed8ea18a5d88fd9b9eeab61f2a7b68e1c8d
hansh0101 <hansh0101@naver.com>
2022-07-18 23:26:33 +0900

    [FEATURE] #95 - TimeMachineChat3Fragment 애니메이션 구현

--- app/src/main/java/co/kr/deartoday/presentation/ui/timemachine/TimeMachineChat3Fragment.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/timemachine/TimeMachineChat3Fragment.kt
@@ -2,13 +2,18 @@ package co.kr.deartoday.presentation.ui.timemachine
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.lifecycle.lifecycleScope
 import co.kr.deartoday.R
 import co.kr.deartoday.databinding.FragmentTimeMachineChat3Binding
 import co.kr.deartoday.presentation.ui.base.BaseFragment
 import co.kr.deartoday.presentation.viewmodel.TimeMachineViewModel
+import co.kr.deartoday.util.fadeInAnimator
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimeMachineChat3Fragment : BaseFragment<FragmentTimeMachineChat3Binding>() {
     override val TAG: String
@@ -22,7 +27,19 @@ class TimeMachineChat3Fragment : BaseFragment<FragmentTimeMachineChat3Binding>()
         super.onViewCreated(view, savedInstanceState)
         binding.viewmodel = viewModel
 
+        initAnimation()
         initOnClickListener()
+    }
+
+    private fun initAnimation() {
+        (requireActivity() as TimeMachineActivity).mainScope.launch {
+            delay(400)
+            fadeInAnimator(binding.tvContent, 300).start()
+            binding.tvContent.isVisible = true
+            delay(900)
+            fadeInAnimator(binding.tvNext, 600).start()
+            binding.tvNext.isVisible = true
+        }
     }
 
     private fun initOnClickListener() {
