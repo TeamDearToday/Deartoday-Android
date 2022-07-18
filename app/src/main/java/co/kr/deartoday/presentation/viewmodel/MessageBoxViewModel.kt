fd245228e721b57101545cb5857b659a4da7c19a
uxri <yuree0217@g.eulji.ac.kr>
2022-07-18 19:14:23 +0900

    [FEATURE] Modify MessageBoxViewModel

--- app/src/main/java/co/kr/deartoday/presentation/viewmodel/MessageBoxViewModel.kt
+++ app/src/main/java/co/kr/deartoday/presentation/viewmodel/MessageBoxViewModel.kt
@@ -1,8 +1,38 @@
 package co.kr.deartoday.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.kr.deartoday.data.ServiceCreator.deartodayService
+import co.kr.deartoday.data.model.response.MessageBoxResponse
+import co.kr.deartoday.util.SingleLiveEvent
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class MessageBoxViewModel : ViewModel() {
-    var lastAnswers = MutableLiveData<String>()
+    private var _data = MutableLiveData<MessageBoxResponse>() //업데이트
+    val data: LiveData<MessageBoxResponse> get() = _data //관찰
+
+    private var _isSuccess = SingleLiveEvent<Boolean>()
+    val isSuccess: LiveData<Boolean> get() = _isSuccess
+
+    private var _isEmpty = SingleLiveEvent<Boolean>()
+    val isEmpty: LiveData<Boolean> get() = _isEmpty
+
+    fun getMessage() {
+        viewModelScope.launch {
+            runCatching {
+                deartodayService.getMessageBox()
+            }
+                .onSuccess {
+                    _data = MutableLiveData(it)
+                    //성공했을 때 뷰모델 정보 저장
+                    _isSuccess.value = true
+                }
+                .onFailure {
+                }
+        }
+        //isEmpty인 경우에 또 처리해주고?
+    }
 }
