ca3e04c730936833328c39ed36930f55821543db
Choi Woo hyeong <gua05093@gmail.com>
2022-07-18 22:33:06 +0900

    [REFACTOR] #93 - Auth / request, response, service 구현

--- app/src/main/java/co/kr/deartoday/data/service/AuthService.kt
+++ app/src/main/java/co/kr/deartoday/data/service/AuthService.kt
@@ -1,6 +1,8 @@
 package co.kr.deartoday.data.service
 
 import co.kr.deartoday.data.model.request.auth.AuthRequest
+import co.kr.deartoday.data.model.response.BaseResponse
+import co.kr.deartoday.data.model.response.auth.AuthResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -10,6 +12,5 @@ interface AuthService {
     suspend fun getAccessToken(
         @Path("social") social: String,
         @Body body: AuthRequest
-
-    )
+    ): BaseResponse<AuthResponse>
 }
