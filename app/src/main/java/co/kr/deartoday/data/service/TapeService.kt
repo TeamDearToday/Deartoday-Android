445be1dcc122f3d3f1ac26e88f1d1e411915f403
Choi Woo hyeong <gua05093@gmail.com>
2022-07-18 22:11:12 +0900

    [REFACTOR] #93 - getTapes suspend로 만듬

--- app/src/main/java/co/kr/deartoday/data/service/TapeService.kt
+++ app/src/main/java/co/kr/deartoday/data/service/TapeService.kt
@@ -5,5 +5,5 @@ import retrofit2.http.GET
 
 interface TapeService {
     @GET("/timeTravel")
-    fun getTapes(): List<TapesResponse>
+    suspend fun getTapes(): List<TapesResponse>
 }
--- app/src/main/java/co/kr/deartoday/presentation/viewmodel/SignInViewModel.kt
+++ app/src/main/java/co/kr/deartoday/presentation/viewmodel/SignInViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import co.kr.deartoday.data.ServiceCreator
 import kotlinx.coroutines.launch
 
 class SignInViewModel : ViewModel() {
