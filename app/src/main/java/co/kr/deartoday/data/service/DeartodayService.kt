32d109c5c7735529cf530a53f862f4e31e3b2f51
Choi Woo hyeong <gua05093@gmail.com>
2022-07-18 14:16:33 +0900

    [REFACTOR] #89 - 서비스 함수 추가

--- app/src/main/java/co/kr/deartoday/data/service/DeartodayService.kt
+++ app/src/main/java/co/kr/deartoday/data/service/DeartodayService.kt
@@ -1,9 +1,12 @@
 package co.kr.deartoday.data.service
 
 import co.kr.deartoday.data.model.response.ResponseMessageBox
+import co.kr.deartoday.data.model.response.TapesResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface DeartodayService {
+    @GET("/timeTravel")
+    fun getTapes(): List<TapesResponse>
 
 }
