dddd7ce055a9e747b377279b75906ddfb4149073
Choi Woo hyeong <gua05093@gmail.com>
2022-07-18 16:51:16 +0900

    [REFACTOR] #89 - 다음 이슈 전 주석수정

--- app/src/main/java/co/kr/deartoday/data/sharedpreferences/DearTodaySharedPreferences.kt
+++ app/src/main/java/co/kr/deartoday/data/sharedpreferences/DearTodaySharedPreferences.kt
@@ -17,8 +17,7 @@ const val FILE_NAME = "DEAR_TODAY_..."
 // 이 주석은 merge 직전에 사라질 예정입니다.
 
 class DearTodaySharedPreferences @Inject constructor(
-    @ApplicationContext private val context: Context    // creat의 인자로서 contxt 가 필요하므로... 아무래도,,,
-    // 근데 어노테이션을 쓰면 좋은 이유 블로그 참조
+    @ApplicationContext private val context: Context
 ) {
     private val dearTodaySharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
         FILE_NAME,
@@ -28,19 +27,6 @@ class DearTodaySharedPreferences @Inject constructor(
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
 
-    //sharedPreferences.edit(){ // Edit the user's shared preferences... }
-    //변수 == 프로퍼티
-
-    fun tokenset(value: String) {
-        dearTodaySharedPreferences.edit { putString("DEAR_TODAY_TOKEN", value) }
-    }
-
-    fun tokenget() {
-        dearTodaySharedPreferences.getString("DEAR_TODAY_TOKEN", "") ?: ""
-    }
-
-    // 변수에 게더와 세터 직접 정의 근데 return this.dearTodayToken 이 아니니까 변수 안에는 값이 있는가?
-    // 무릎을 탁 쳤는데 약간 꼼수? 인데 엄청 고급지고 활용하기 좋은 정석이 되는듯한,,,?!
     var dearTodayToken: String
         set(value) = dearTodaySharedPreferences.edit { putString("DEAR_TODAY_TOKEN", value) }
         get() = dearTodaySharedPreferences.getString("DEAR_TODAY_TOKEN", "") ?: ""
