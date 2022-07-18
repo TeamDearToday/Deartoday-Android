4540573abffb7d353f130984f92b0442567dbd72
Choi Woo hyeong <gua05093@gmail.com>
2022-07-18 14:31:45 +0900

    [REFACTOR] #89 - TapeAdapter 클릭리스너 구현

--- app/src/main/java/co/kr/deartoday/presentation/adapter/TapeAdapter.kt
+++ app/src/main/java/co/kr/deartoday/presentation/adapter/TapeAdapter.kt
@@ -6,14 +6,14 @@ import androidx.recyclerview.widget.RecyclerView
 import co.kr.deartoday.data.model.response.TapesResponse
 import co.kr.deartoday.databinding.ItemTimeTravelBinding
 
-class TapeAdapter :
+class TapeAdapter(private val itemClick: (TapesResponse.Tape) -> (Unit)) :
     RecyclerView.Adapter<TapeAdapter.TimeTravelTapeViewHolder>() {
     val tapeList = mutableListOf<TapesResponse.Tape>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeTravelTapeViewHolder {
         val binding =
             ItemTimeTravelBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return TimeTravelTapeViewHolder((binding))
+        return TimeTravelTapeViewHolder(binding, itemClick)
     }
 
     override fun onBindViewHolder(holder: TimeTravelTapeViewHolder, position: Int) {
@@ -23,12 +23,16 @@ class TapeAdapter :
     override fun getItemCount(): Int = tapeList.size
 
     class TimeTravelTapeViewHolder(
-        private val binding: ItemTimeTravelBinding
+        private val binding: ItemTimeTravelBinding,
+        private val itemClick: (TapesResponse.Tape) -> (Unit)
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: TapesResponse.Tape) {
             binding.content.text = data.title
             binding.tvTravelFrom.text = data.writtenDate
             binding.tvTravelTo.text = "%d.%d.%d".format(data.year, data.month, data.day)
+            binding.root.setOnClickListener {
+                itemClick(data)
+            }
         }
     }
 }
