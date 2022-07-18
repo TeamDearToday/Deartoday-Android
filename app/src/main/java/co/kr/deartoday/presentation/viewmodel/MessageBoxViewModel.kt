a654f3eb7f9be42bda3e49b791606f20265578a8
uxri <yuree0217@g.eulji.ac.kr>
2022-07-18 23:03:49 +0900

    [FEATURE] #75 - MessageBoxViewModel message 가져오는 서버통신 구현

---
+++ app/src/main/java/co/kr/deartoday/presentation/viewmodel/MessageBoxViewModel.kt
@@ -0,0 +1,36 @@
+package co.kr.deartoday.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.kr.deartoday.data.ServiceCreator
+import co.kr.deartoday.data.model.response.MessageBoxResponse
+import co.kr.deartoday.util.SingleLiveEvent
+import kotlinx.coroutines.launch
+
+class MessageBoxViewModel : ViewModel() {
+    private var _data = MutableLiveData<MessageBoxResponse>() //업데이트
+    val data: LiveData<MessageBoxResponse> get() = _data //관찰
+
+    private var _isSuccess = SingleLiveEvent<Boolean>()
+    val isSuccess: LiveData<Boolean> get() = _isSuccess
+
+    private var _isEmpty = SingleLiveEvent<Boolean>()
+    val isEmpty: LiveData<Boolean> get() = _isEmpty
+
+    fun getMessage() {
+        viewModelScope.launch {
+            runCatching {
+                ServiceCreator.messageBoxService.getMessageBox()
+            }
+                .onSuccess {
+                    //성공했을 때 뷰모델 정보 저장
+                    _isSuccess.value = true
+                }
+                .onFailure {
+                }
+        }
+        //isEmpty인 경우에 또 처리해주고?
+    }
+}
