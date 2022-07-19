c62747c4816a35b3590e1f7ac354c7ca513025f2
hansh0101 <hansh0101@naver.com>
2022-07-19 23:31:53 +0900

    [RENAME] #109 - 시간여행 가상공간 질문 가져오기 API 연결

--- app/src/main/java/co/kr/deartoday/data/service/timemachine/TimeMachineService.kt
+++ app/src/main/java/co/kr/deartoday/data/service/timemachine/TimeMachineService.kt
@@ -2,6 +2,7 @@ package co.kr.deartoday.data.service.timemachine
 
 import co.kr.deartoday.data.model.response.BaseResponse
 import co.kr.deartoday.data.model.response.timemachine.PastImagesResponse
+import co.kr.deartoday.data.model.response.timemachine.QuestionsResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,4 +11,7 @@ interface TimeMachineService {
     suspend fun fetchPastImages(
         @Query("year") year: Int
     ): BaseResponse<PastImagesResponse>
+
+    @GET("timeTravel/question")
+    suspend fun fetchQuestions(): BaseResponse<QuestionsResponse>
 }
