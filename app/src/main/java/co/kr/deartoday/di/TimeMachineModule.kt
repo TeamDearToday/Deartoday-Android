746bb4d90ba850257db45dff4cce359948d97c74
hansh0101 <hansh0101@naver.com>
2022-07-19 22:21:39 +0900

    [FEATURE] #106 - 시간여행 가상공간 - 특정연도, 사진 가져오기 API 연결

---
+++ app/src/main/java/co/kr/deartoday/di/TimeMachineModule.kt
@@ -0,0 +1,28 @@
+package co.kr.deartoday.di
+
+import co.kr.deartoday.data.repository.TimeMachineRepositoryImpl
+import co.kr.deartoday.data.service.TimeMachineService
+import co.kr.deartoday.domain.repository.timemachine.TimeMachineRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TimeMachineModule {
+    @Provides
+    @Singleton
+    fun provideTimeMachineService(retrofit: Retrofit): TimeMachineService =
+        retrofit.create(TimeMachineService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideTimeMachineRepository(
+        timeMachineService: TimeMachineService,
+        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
+    ): TimeMachineRepository = TimeMachineRepositoryImpl(timeMachineService, coroutineDispatcher)
+}
