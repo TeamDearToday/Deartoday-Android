f926c508cccfa40fa9c88b86faa33480ee478f63
uxri <yuree0217@g.eulji.ac.kr>
2022-07-19 22:34:17 +0900

    [ADD] #107 - Splash package 생성

--- app/src/main/java/co/kr/deartoday/presentation/ui/splash/DeartodaySplashActivity.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/splash/DeartodaySplashActivity.kt
@@ -1,4 +1,4 @@
-package co.kr.deartoday.presentation.ui.onboarding
+package co.kr.deartoday.presentation.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,7 @@ import android.view.View
 import co.kr.deartoday.R
 import co.kr.deartoday.databinding.ActivityDeartodaySplashBinding
 import co.kr.deartoday.presentation.ui.base.BaseActivity
+import co.kr.deartoday.presentation.ui.onboarding.OnboardingActivity
 import co.kr.deartoday.util.fadeInAnimator
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
