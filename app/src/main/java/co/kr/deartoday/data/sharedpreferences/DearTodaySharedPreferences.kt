6292baacad85c7ee7a1434391f2ad6228935b482
uxri <yuree0217@g.eulji.ac.kr>
2022-07-20 02:12:38 +0900

    [CHORE] #117 - 안 쓰는 import문 정리

--- app/src/main/java/co/kr/deartoday/data/sharedpreferences/DearTodaySharedPreferences.kt
+++ app/src/main/java/co/kr/deartoday/data/sharedpreferences/DearTodaySharedPreferences.kt
@@ -1,7 +1,6 @@
 package co.kr.deartoday.data.sharedpreferences
 
 import android.content.Context
-import android.content.SharedPreferences
 import androidx.core.content.edit
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
