acce74261e8624cf2fc598334081541d2c2c828c
hansh0101 <hansh0101@naver.com>
2022-07-20 02:05:30 +0900

    [FEATURE] #105 - MultiPart 통신 구현

--- app/src/main/java/co/kr/deartoday/presentation/viewmodel/timemachine/TimeMachineViewModel.kt
+++ app/src/main/java/co/kr/deartoday/presentation/viewmodel/timemachine/TimeMachineViewModel.kt
@@ -1,16 +1,27 @@
 package co.kr.deartoday.presentation.viewmodel.timemachine
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import androidx.lifecycle.*
 import co.kr.deartoday.domain.repository.timemachine.TimeMachineRepository
+import co.kr.deartoday.presentation.usecase.TimeMachineUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okio.BufferedSink
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class TimeMachineViewModel @Inject constructor(
-    private val timeMachineRepository: TimeMachineRepository
+    private val timeMachineRepository: TimeMachineRepository,
+    private val timeMachineUseCase: TimeMachineUseCase
 ) : ViewModel() {
     // TimeMachineImagePickerFragment
     val imageUri = MutableLiveData<Uri>()
@@ -36,6 +47,11 @@ class TimeMachineViewModel @Inject constructor(
     private var _lastMessages = listOf<String>()
     val lastMessages get() = _lastMessages
     val answer = MutableLiveData<String>()
+
+    var bitmap: Bitmap? = null
+
+    val _isSuccess = MutableLiveData<Boolean>()
+    val isSuccess: LiveData<Boolean> get() = _isSuccess
 
     init {
         with(_isImagePickerProcessComplete) {
@@ -84,7 +100,73 @@ class TimeMachineViewModel @Inject constructor(
         }
     }
 
+    fun postTimeTravel() {
+        // title: String,
+        // year, month, day: Int,
+        // image: MultipartBody.Part
+        // writtenDate: String
+        // questions[0] ~ questions[6]: String
+        // answers[0] ~ answers[6]: String
+        val requestBodyHashMap = HashMap<String, RequestBody>()
+        val titleRequestBody =
+            requireNotNull(title.value).toRequestBody("text/plain".toMediaTypeOrNull())
+        val yearRequestBody = year.toRequestBody("text/plain".toMediaTypeOrNull())
+        val monthRequestBody = month.toRequestBody("text/plain".toMediaTypeOrNull())
+        val dayRequestBody = day.toRequestBody("text/plain".toMediaTypeOrNull())
+        val writtenDateRequestBody =
+            requireNotNull(date.value).toRequestBody("text/plain".toMediaTypeOrNull())
+        requestBodyHashMap["title"] = titleRequestBody
+        requestBodyHashMap["year"] = yearRequestBody
+        requestBodyHashMap["month"] = monthRequestBody
+        requestBodyHashMap["day"] = dayRequestBody
+        requestBodyHashMap["writtenDate"] = writtenDateRequestBody
+
+        for (i in 0..6) {
+            val question = if (i == 6) {
+                "마지막으로, 과거의 당신에게 꼭 해주고 싶은 말을 남기세요."
+            } else {
+                questions[i]
+            }
+            val answer = answers[i]
+            val questionRequestBody = question.toRequestBody("text/plain".toMediaTypeOrNull())
+            val answerRequestBody = answer.toRequestBody("text/plain".toMediaTypeOrNull())
+            requestBodyHashMap["questions[$i]"] = questionRequestBody
+            requestBodyHashMap["answers[$i]"] = answerRequestBody
+        }
+
+
+        val imageRequestBody = BitmapRequestBody(requireNotNull(bitmap))
+        val imageMultipartBody: MultipartBody.Part =
+            MultipartBody.Part.createFormData(
+                "image",
+                "and" + System.currentTimeMillis().toString(),
+                imageRequestBody
+            )
+
+        viewModelScope.launch {
+            timeMachineUseCase.postTimeTravel(imageMultipartBody, requestBodyHashMap)
+                .onSuccess {
+                    _isSuccess.value = true
+                }.onFailure {
+                    _isSuccess.value = false
+                    Timber.e(it)
+                }
+        }
+    }
+
     fun addAnswer(answer: String) {
         answers.add(answer)
+    }
+
+    companion object {
+        class BitmapRequestBody(private val bitmap: Bitmap) : RequestBody() {
+            override fun contentType(): MediaType? {
+                return "image/png".toMediaTypeOrNull()
+            }
+
+            override fun writeTo(sink: BufferedSink) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 99, sink.outputStream()) //99프로 압축
+            }
+        }
     }
 }
