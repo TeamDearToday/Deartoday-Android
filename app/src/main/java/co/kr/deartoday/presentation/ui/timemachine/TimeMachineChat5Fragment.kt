1bc26a635581b79162001988de6756bfcbfb3fa5
hansh0101 <hansh0101@naver.com>
2022-07-20 00:11:42 +0900

    [FEATURE] #113 - setOnSingleClickListener로 교체

--- app/src/main/java/co/kr/deartoday/presentation/ui/timemachine/TimeMachineChat5Fragment.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/timemachine/TimeMachineChat5Fragment.kt
@@ -10,6 +10,7 @@ import co.kr.deartoday.presentation.ui.base.BaseFragment
 import co.kr.deartoday.presentation.viewmodel.timemachine.TimeMachineViewModel
 import co.kr.deartoday.util.fadeInAnimator
 import co.kr.deartoday.util.fadeOutAnimator
+import co.kr.deartoday.util.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -31,7 +32,10 @@ class TimeMachineChat5Fragment : BaseFragment<FragmentTimeMachineChat5Binding>()
     }
 
     private fun initOnClickListener() {
-        binding.tvNext.setOnClickListener {
+        binding.ivExit.setOnSingleClickListener {
+            requireActivity().onBackPressed()
+        }
+        binding.tvNext.setOnSingleClickListener {
             (requireActivity() as TimeMachineActivity).mainScope.launch {
                 fadeOutAnimator(binding.root, 1000).start()
                 delay(2000)
