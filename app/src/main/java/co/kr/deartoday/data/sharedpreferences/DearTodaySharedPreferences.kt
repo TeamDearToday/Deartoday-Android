51c79844c79b4bfa6529e0dc4c402732dcc22493
hansh0101 <hansh0101@naver.com>
2022-07-19 17:10:37 +0900

    [FEATURE] #97 - SharedPreference Debug 여부에 따라 암호화 처리 구현

--- app/src/main/java/co/kr/deartoday/data/sharedpreferences/DearTodaySharedPreferences.kt
+++ app/src/main/java/co/kr/deartoday/data/sharedpreferences/DearTodaySharedPreferences.kt
@@ -5,27 +5,27 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
+import co.kr.deartoday.BuildConfig
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-const val FILE_NAME = "DEAR_TODAY_..."
-
-// TODO
-// sharedpreferences 를 파일로 만들고 키-값 쌍들을 만들겠죠?
-// 아무래도 deartoday token 값을 넣거나 삭제할 수 있어야 하겠죠?
-// 추후에 sharedprferences token 값이 있거나 없을 경우 -> 자동 로그인 or 뭐 기타등등 해야겠죠?
-// 이 주석은 merge 직전에 사라질 예정입니다.
+const val FILE_NAME = "DEAR_TODAY"
 
 class DearTodaySharedPreferences @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
-    private val dearTodaySharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
-        FILE_NAME,
-        MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
-        context,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
+    private val dearTodaySharedPreferences =
+        if (BuildConfig.DEBUG) {
+            context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        } else {
+            EncryptedSharedPreferences.create(
+                FILE_NAME,
+                MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
+                context,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+        }
 
     var dearTodayToken: String
         set(value) = dearTodaySharedPreferences.edit { putString("DEAR_TODAY_TOKEN", value) }
@@ -34,5 +34,4 @@ class DearTodaySharedPreferences @Inject constructor(
     var autoLogin: Boolean
         set(value) = dearTodaySharedPreferences.edit { putBoolean("AUTO_LOGIN", value) }
         get() = dearTodaySharedPreferences.getBoolean("AUTO_LOGIN", false)
-
 }
