19de96d031ef34659f9af61e00481e927b27ae08
uxri <yuree0217@g.eulji.ac.kr>
2022-07-21 03:50:27 +0900

    [FEATURE] yr/#129 - Onboarding 백버튼 2회 누르면 종료 구현

--- app/src/main/java/co/kr/deartoday/presentation/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/onboarding/OnboardingActivity.kt
@@ -3,11 +3,19 @@ package co.kr.deartoday.presentation.ui.onboarding
 import co.kr.deartoday.R
 import co.kr.deartoday.databinding.ActivityOnboardingBinding
 import co.kr.deartoday.presentation.ui.base.BaseActivity
+import co.kr.deartoday.util.shortToast
 
 class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>() {
     override val layoutRes: Int
         get() = R.layout.activity_onboarding
 
+    private var backPressedTime: Long = 0
     override fun onBackPressed() {
+        if (System.currentTimeMillis() - backPressedTime < 2000) {
+            finish()
+            return
+        }
+        shortToast("암온더넥스트레벨.\n코틀린문법지켜.")
+        backPressedTime = System.currentTimeMillis()
     }
 }
