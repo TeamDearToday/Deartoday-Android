78e2633a54354fea68fd30659784ee3d85154bc1
hansh0101 <hansh0101@naver.com>
2022-07-22 22:53:54 +0900

    [REFACTOR] #144 - Hilt 적용

--- app/src/main/java/co/kr/deartoday/presentation/viewmodel/messagebox/MessageBoxViewModel.kt
+++ app/src/main/java/co/kr/deartoday/presentation/viewmodel/messagebox/MessageBoxViewModel.kt
@@ -8,15 +8,19 @@ import co.kr.deartoday.data.service.messagebox.MessageBoxService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
-class MessageBoxViewModel : ViewModel() {
+@HiltViewModel
+class MessageBoxViewModel @Inject constructor(
+    private val messageBoxService: MessageBoxService
+) : ViewModel() {
     private val _lastAnswer = MutableLiveData<List<String>>() //업데이트
     val lastAnswer: LiveData<List<String>> get() = _lastAnswer //관찰
 
     fun getMessage() {
         viewModelScope.launch {
             runCatching {
-                ServiceCreator.messageBoxService.getMessageBox()
+                messageBoxService.getMessageBox()
             }.onSuccess {
                 _lastAnswer.value = it.data.lastAnswer
             }.onFailure {
