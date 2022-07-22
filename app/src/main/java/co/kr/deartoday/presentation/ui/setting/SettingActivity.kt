3912e07667c8af58310b709d4dcdb4c7804b9dc6
hansh0101 <hansh0101@naver.com>
2022-07-22 22:54:06 +0900

    [REFACTOR] #144 - Hilt 적용

--- app/src/main/java/co/kr/deartoday/presentation/ui/setting/SettingActivity.kt
+++ app/src/main/java/co/kr/deartoday/presentation/ui/setting/SettingActivity.kt
@@ -2,29 +2,34 @@ package co.kr.deartoday.presentation.ui.setting
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import co.kr.deartoday.R
-import co.kr.deartoday.data.sharedpreferences.DearTodaySharedPreferences
 import co.kr.deartoday.databinding.ActivitySettingBinding
 import co.kr.deartoday.presentation.ui.base.BaseActivity
-import co.kr.deartoday.presentation.ui.onboarding.OnboardingActivity
+import co.kr.deartoday.presentation.ui.splash.DeartodaySplashActivity
+import co.kr.deartoday.presentation.viewmodel.setting.SettingViewModel
 import co.kr.deartoday.util.shortToast
-import timber.log.Timber
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingActivity : BaseActivity<ActivitySettingBinding>() {
     override val layoutRes: Int
         get() = R.layout.activity_setting
+
+    private val viewModel by viewModels<SettingViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         backBtnClickEvent()
         logoutClickEvent()
+        observeData()
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
-        overridePendingTransition(R.anim.not_move,R.anim.right_out)
+        overridePendingTransition(R.anim.not_move, R.anim.right_out)
     }
 
     private fun backBtnClickEvent() {
@@ -41,11 +46,16 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
     }
 
     private fun logout() {
-        DearTodaySharedPreferences(this).dearTodayToken = ""
-        Timber.v("최유리/토큰 확인[${DearTodaySharedPreferences(this).dearTodayToken}]")
-        val intent = Intent(this, OnboardingActivity::class.java)
-        ActivityCompat.finishAffinity(this)
-        startActivity(intent)
-        shortToast("로그아웃 되었습니다")
+        viewModel.logout()
+    }
+
+    private fun observeData() {
+        viewModel.isLogoutSuccess.observe(this) {
+            if (it) {
+                startActivity(Intent(this, DeartodaySplashActivity::class.java))
+                ActivityCompat.finishAffinity(this)
+                shortToast("로그아웃 되었습니다")
+            }
+        }
     }
 }
